a4e688e6ee00fe3d5d595751f512a1a1a60f0fc0
Miquel <mpetrus@iesjoanramis.org>
2023-10-17 16:37:17 +0200

    Commit per fer un revert

--- programa1.java
+++ programa1.java
@@ -1,7 +1,10 @@
 //Programa de prova per usar git
 //Modificació
+<<<<<<< HEAD
 //Modificacio #2
 // Afegim els comentaris des de la branca de comentaris
 //Afegit un comentari des de la branca master
 //Comentaris afegits des de la branca de modiciacio
 
+=======
+>>>>>>> parent of 05c7c6e (Dues modificacions)
