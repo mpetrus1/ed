9b50d87b2c77df0fd9ea2eb9679542c715b3f9c5
Miquel <mpetrus@iesjoanramis.org>
2023-10-17 16:40:28 +0200

    Revert

--- programa1.java
+++ programa1.java
@@ -1,10 +1,6 @@
 //Programa de prova per usar git
 //Modificació
-<<<<<<< HEAD
 //Modificacio #2
 // Afegim els comentaris des de la branca de comentaris
 //Afegit un comentari des de la branca master
-//Comentaris afegits des de la branca de modiciacio
-
-=======
->>>>>>> parent of 05c7c6e (Dues modificacions)
+//Comentaris afegits des de la branca de modiciaci
